1a2b1a2dfb6dc35396cae74ecb17096b3fb32c17
Joe Gdaniec <jgdaniec@redhat.com>
2020-06-04 08:25:27 -0400

    Add cover scripts for version-related redirection (#76)

---
+++ tools/bundle-manifests-gen/gen-unbound-acm-bundle.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Cover script that redirects to release-specific version of script assumed
+# to be  named like this one (with  a release-qualifier suffix) and that lives
+# in the same directory as this one.
+#
+# Assumes:
+# - The script name(s) ends with a ".sh" suffix.
+#
+# Note: This simple-minded redirect works because the underlying scripts don't
+# have any getopt options to worry about.
+
+me=${0##*/}
+me_pfx=${0%$me}
+
+new_csv_vers="$1"
+if [[ -z "$new_csv_vers" ]]; then
+   >&2 echo "Error: Bundle version is required."
+   exit 1
+fi
+
+old_IFS=$IFS
+IFS=. rel_xyz=(${new_csv_vers%-*})
+rel_x=${rel_xyz[0]}
+rel_y=${rel_xyz[1]}
+rel_z=${rel_xyz[2]}
+IFS=$old_IFS
+
+rel_xy="$rel_x.$rel_y"
+
+rel_qualifier="1.0"
+
+target_script="$me_pfx${me%.sh}-$rel_qualifier.sh"
+exec $target_script "$@"
+
