bbafc0ae531f732881b7860128e05d131975df41
Joe Gdaniec <59924162+joeg-pro@users.noreply.github.com>
2021-07-19 17:19:57 -0400

    Fix bug in channel-override logica and temp enable overrides for ACM 2.4 (#222)

--- tools/bundle-manifests-gen/gen-unbound-acm-bundle.sh
+++ tools/bundle-manifests-gen/gen-unbound-acm-bundle.sh
@@ -52,9 +52,9 @@ prev_csv_vers="$2"
 
 appsub_use_previous_release_channel_override=0
 hive_use_previous_release_channel_override=0
-if [[ "$new_csv_vers" == "2.3.0" ]]; then
-   # No oerrides.
-   true
+if [[ "$new_csv_vers" == "2.4.0" ]]; then
+   appsub_use_previous_release_channel_override=1
+   hive_use_previous_release_channel_override=1
 fi
 
 parse_release_nr "$new_csv_vers"
@@ -128,7 +128,7 @@ bundle_dirs["OCM Hub"]="$bundle_dir"
 # -- App Sub --
 
 locate_community_operator "App Sub" "multicluster-operators-subscription" "release" "$rel_xy" \
-   "${app_sub_source_csv_vers:-none}" "${app_sub_use_previous_release_channel_override:-0}"
+   "${app_sub_source_csv_vers:-none}" "${appsub_use_previous_release_channel_override:-0}"
 
 # -- Hive --
 
