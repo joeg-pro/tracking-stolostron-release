7d7d250136e4bf4423f145ded528ef87aa694b2d
Joe Gdaniec <59924162+joeg-pro@users.noreply.github.com>
2021-09-13 17:49:13 -0400

    custom-registry-gen:  Update to use OPM version 1.17.5 (#259)

--- tools/custom-registry-gen/gen-custom-registry.sh
+++ tools/custom-registry-gen/gen-custom-registry.sh
@@ -6,7 +6,7 @@
 # requires:
 # jq (no reasonably involvd script can live without it).
 
-opm_vers="v1.13.3"
+opm_vers="v1.17.5"
 
 operator_rgy_repo_url="https://github.com/operator-framework/operator-registry"
 opm_download_url="$operator_rgy_repo_url/releases/download/$opm_vers/linux-amd64-opm"
